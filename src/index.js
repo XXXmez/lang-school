c9661f2d7fed084f6e42328d8f54dd2be1cfa883
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-08-23 16:51:51 +0300

    init: layout
    Made a footer and a section with developers, more or less made a general layout, started making a slider

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import "./file/img/logo.svg"
 import "./file/img/1.png"
 
 console.log('Hi');
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    direction: 'vertical',
+    loop: true,
+});
