b2045688120ce1e3bf4b3eaf30e2f200f656a84f
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-08-24 16:44:23 +0300

    init:  slider section
    Completed the section with the slider, made the layout of the section, the layout of the slider, set up the correct operation of the script

--- src/index.js
+++ src/index.js
@@ -15,6 +15,19 @@ import 'swiper/css/pagination';
 const swiper = new Swiper('.swiper', {
     // configure Swiper to use modules
     modules: [Navigation, Pagination],
-    direction: 'vertical',
-    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            let nameBullet = '';
+            if (index === 0) nameBullet = 'Beginner';
+            if (index === 1) nameBullet = 'Elementary';
+            if (index === 2) nameBullet = 'Pre-Intermediate';
+            if (index === 3) nameBullet = 'Intermediate';
+            if (index === 4) nameBullet = 'Upper-Intermediate';
+
+            return '<span class="swiper-label ' + className + '">' + (nameBullet) + '</span>';
+        },
+    },
+    speed: 900,
 });
